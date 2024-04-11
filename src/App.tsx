c0e7c4ae97ba741ee726c9872f1a91a442da5ae1
Jean Vera Soto <jeanvs23@gmail.com>
2024-04-11 13:44:35 +1000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Under maintenance</h1>
+      <p className="read-the-docs">
+        
+      </p>
+    </>
+  )
+}
+
+export default App
